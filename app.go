1b5bbe834856624ee79a23e9a64b18a3ade3c7f5
cyrus01337 <cyrus01337@gmail.com>
2026-06-30 00:01:46 +0100

    Add boilerplate

--- app.go
+++ app.go
@@ -1,9 +1,32 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"net/http"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
+func serveHomePage(writer http.ResponseWriter, request *http.Request) {
+	body, error := os.ReadFile("index.html")
+
+	if error != nil {
+		body = []byte(error.Error())
+	}
+
+	fmt.Fprintf(writer, string(body))
+}
+
 func main() {
-	os.Exit(0)
+	port := os.Getenv("PORT")
+
+	if err := godotenv.Load(".env"); err != nil {
+		port = "3000"
+	}
+
+	http.HandleFunc("/", serveHomePage)
+	fmt.Printf("Listening to localhost:%s...\n", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
